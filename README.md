4400bc5b80ad63a1880f60df4ddcb439d91b885b
Samir <samirmh15@gmail.com>
2024-10-17 01:23:51 +0200

    Se crearon models Stores, Products y Employees ademas que se instalo dotenv

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Debe ejecutar los comandos
 npm install express
 npm install --save-dev nodemon
 npm install dotenv
+npm install mongoose
+
+crear archivo .env y configurar
+PORT = 8080
+
---
+++ models/Employees.js
@@ -0,0 +1,13 @@
+import {Schema,model} from "mongoose";
+
+let collection = 'employees' 
+let schema = new Schema({
+    name:{type:String,required:true},
+    post:{type:String,required:true},
+    salary:{Number:String,required:true}
+},{
+    timestamps:true
+})
+
+let Employee = model(collection,schema)
+export default Employee
