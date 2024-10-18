1a344524256bae920cacb36071f6af3caf67559d
Samir <samirmh15@gmail.com>
2024-10-18 03:22:48 +0200

    Se crearon 2 endponints adicionales para el model de products

--- controllers/products/read.js
+++ controllers/products/read.js
@@ -1,5 +1,35 @@
 import Product from "../../models/Products.js";
 
+let typeProducts =  async (req,res) => {
+    try {
+        let typeQuery = req.params.typePro
+        let type = await Product.find({type:typeQuery})
+        return res.status(200).json({
+            response:type
+        })
+    } catch (error) {
+       return response.status(500).json({
+        response: error
+       }) 
+    }
+
+}
+
+let nameProducts =  async (req,res) => {
+    try {
+        let nameQuery = req.params.namePro
+        let name = await Product.find({name:nameQuery})
+        return res.status(200).json({
+            response:name
+        })
+    } catch (error) {
+       return response.status(500).json({
+        response: error
+       }) 
+    }
+
+}
+
 let allProducts =  async (req,res) => {
     try {
         let all = await Product.find()
@@ -14,4 +44,4 @@ let allProducts =  async (req,res) => {
 
 }
  
-export default allProducts
+export {allProducts, nameProducts, typeProducts}
--- router/products.js
+++ router/products.js
@@ -1,8 +1,10 @@
 import {Router} from "express";
-import allProducts from "../controllers/products/read.js";
+import {allProducts, typeProducts, nameProducts} from "../controllers/products/read.js";
 
 const router = Router()
 
 router.get('/all',allProducts)
+router.get('/type/:typePro',typeProducts)
+router.get('/name/:namePro',nameProducts)
 
 export default router
