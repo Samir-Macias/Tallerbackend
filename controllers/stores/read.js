e901787c98fc86843b2c7a35b000b06bd886f932
Samir <samirmh15@gmail.com>
2024-10-18 03:32:23 +0200

    Se crearon 2 endponints adicionales para el model de products

--- controllers/stores/read.js
+++ controllers/stores/read.js
@@ -14,4 +14,34 @@ let allStores = async (req,res) => {
 
 }
 
-export default allStores
+let nameStores = async (req,res) => {
+    try {
+        let nameQuery = req.params.nameEst
+        let name = await Store.find({name: nameQuery})
+        return res.status(200).json({
+            response:name
+        })
+    } catch (error) {
+       return response.status(500).json({
+        response: error
+       }) 
+    }
+
+}
+
+let addressStores = async (req,res) => {
+    try {
+        let addressQuery = req.params.addressEst
+        let address = await Store.find({address: addressQuery})
+        return res.status(200).json({
+            response:address
+        })
+    } catch (error) {
+       return response.status(500).json({
+        response: error
+       }) 
+    }
+
+}
+
+export {allStores, addressStores, nameStores} 
--- router/stores.js
+++ router/stores.js
@@ -1,8 +1,10 @@
 import {Router } from "express";
-import allStores from "../controllers/stores/read.js";
+import {allStores, nameStores, addressStores} from "../controllers/stores/read.js";
 
 const router = Router()
 
 router.get('/all',allStores)
+router.get('/name/:nameEst',nameStores)
+router.get('/address/:addressEmp',addressStores)
 
 export default router
