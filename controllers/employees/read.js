c14309c12f5eaab112d755510e568cf93fc961e9
Samir <samirmh15@gmail.com>
2024-10-18 03:03:34 +0200

    Se crearon 2 endponints adicionales para el model de employees

--- controllers/employees/read.js
+++ controllers/employees/read.js
@@ -15,4 +15,32 @@ let allEmployees =  async (req,res) => {
 
 }
 
-export default allEmployees
+let nameEmployees = async (req,res) => {
+    try {
+        let nameQuery = req.params.nameEmp
+        let  name= await Employee.find({name:nameQuery})
+        return res.status(200).json({
+            response:name
+        })
+    } catch (error) {
+       return response.status(500).json({
+        response: error
+       }) 
+    }
+}
+
+let postEmployees = async (req,res) => {
+    try {
+        let postQuery = req.params.postEmp
+        let  post= await Employee.find({post:postQuery})
+        return res.status(200).json({
+            response:post
+        })
+    } catch (error) {
+       return response.status(500).json({
+        response: error
+       }) 
+    }
+}
+
+export {allEmployees, nameEmployees,postEmployees}
--- router/employees.js
+++ router/employees.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import allEmployees from "../controllers/employees/read.js";
+import {allEmployees, nameEmployees, postEmployees } from "../controllers/employees/read.js";
 
 const router = Router()
 
 router.get('/all',allEmployees)
+router.get('/name/:nameEmp',nameEmployees)
+router.get('/post/:postEmp',postEmployees)
 
 export default router
